6f95536fca86d870fb9162fabb159b5d6b4b9d45
SND\piotrw_cp <SND\piotrw_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2008-04-05 16:15:58 +0000

    Added more missing files.
    git-svn-id: https://simpleai.svn.codeplex.com/svn@2655 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ SimpleAI/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleAI")]
+[assembly: AssemblyProduct("SimpleAI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ba6d20e-c43e-4950-838c-3963f2e4e439")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
